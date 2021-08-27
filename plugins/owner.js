b2ab5a9b356d040b72ae5ea2510ece9761500e81
Raku Ichijou <88374559+ZeroChanBot@users.noreply.github.com>
2021-08-27 21:55:52 +0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,5 +1,6 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
+  // Kalo mau tanya² jgn dlu, hp gw lagi rusak :v
   // F this.sendContact(m.chat, '6283144570377', 'RakuChan', m)
 stc = conn.sendContact(m.chat, '6287834993722', 'RakuChan', m)
   conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Developer Z E R O  B O T *', 'status@broadcast')
