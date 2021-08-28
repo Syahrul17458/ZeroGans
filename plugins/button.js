d602d3234491c27a2673b68422c647c3eadc0665
Raku Ichijou <88374559+ZeroChanBot@users.noreply.github.com>
2021-08-28 20:55:22 +0700

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -1,58 +1,25 @@
-let handler = async (m, { conn }) =>
-conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
-                    "listMessage":  {
-                        "title": "HALO KAK\nSaya ZyxMapLeBOT\nPilih Menu Di Bawah Yaa\nDan Jangan Lupa Baca Dibawah ini\n\nSpam = Banned\nTelp = Blok\n\n\nJoin Group WhatsApp\nhttps://bit.ly/36cLg9x\n\nSubscribe Me\nhttps://bit.ly/3hg3bm4",
-                        "description": "*©ZyxMapLe*",
-                        "buttonText": "Click Here To List",
-                        "listType": "SINGLE_SELECT",
-                        "sections": [
-                            {
-                             "rows": [
-                                    {
-                                        "title": `Download Menu`,
-                                        "description": "\nI Want To Use Download Menu",
-                                        "rowId": ".downloadmenu"
-                                    },{
-                                        "title": "Database Menu",
-                                        "description": "\nI Want To Use Database Menu",
-                                        "rowId": ".dbmenu"                 
-                                    },{
-                                        "title": "Button Menu",
-                                        "description": "\nI Want To Use Button Menu",
-                                        "rowId": ".button"                 
-                                    },{
-                                    	"title": "All Menu",
-                                        "description": "\nI Want To Use All Menu",
-                                        "rowId": ".allmenu"                 
-                                    },{
-                                        "title": "Jadibot Menu",
-                                        "description": "\nI Want To Use Jadibot Menu",
-                                        "rowId": ".jadibotmenu"                 
-                                    },{
-                                        "title": "Owner Menu",
-                                        "description": "\nI Want To Use Owner Menu",
-                                        "rowId": ".ownermenu"                 
-                                    },{
-                                        "title": "Hentai Menu",
-                                        "description": "\nI Want To Use Hentai Menu",
-                                        "rowId": ".hentaimenu"                 
-                                    },{
-                                        "title": "Absen Menu",
-                                        "description": "\nI Want To Use Absen Menu",
-                                        "rowId": ".absenmenu"                 
-                                    }, {
-                                        "title": " Group Setting",
-                                        "description": "\nI Want To User Group Setting",
-                                        "rowId": ".groupset"
-                                    }
-                                ]
-                            }
-                        ], "contextInfo": { "stanzaId": m.key.id,
-"participant": m.sender,
-"quotedMessage": m.message
-}
-                    }
-                 }, {}), {waitForAck: true})
-handler.command = ['menu']
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn }) => {
+let { name, limit, level, role, age, money, healt, premium, registered } = global.DATABASE.data.users[m.sender] 
+pp = await conn.getProfilePicture(global.conn.user.jid)
+conn.sendButtonImg(m.chat, 'Pilih Menu Di Bawah Ya Kak\nDan Jangan Lupa Baca Rules Dibawah Ini\n\nSpam = Banned\nTelp = Blok\n\nSubscribe Me\nhttps://bit.ly/3hg3bm4\n\nJoin My Grup WhatsApp Official\nhttps://bit.ly/2VUSJIC\n\n\nMy Rest Api\n1. https://zergans-api.herokuapp.com\n2. https://rakugans.herokuapp.com', 'https://i.ibb.co/fHDx30X/20210725-125918.jpg', `▷Registered : ${registered ? 'Yes': 'No'}\n▷Name : ${name}\n▷Age : ${age}\n▷Ticket : ${limit}\n▷Role : ${role}\n▷Money : ${money}\n▷Level : ${level}\n▷Health : ${healt}\n▷Premium : ${premium ? 'Yes': 'No'}\n\n Thanks To\nNurutomo\nRendyCraft\nIbnu NR\nCaliph71\nRaku-Kun\nAria\nAnd All Creator Bot`.trim(), 'Menu Bot', '.newmenu',m)
+  }
+handler.help = ['menu']
+handler.tags = ['main'] 
+handler.command = /^(menu)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
 handler.register = true
+
+handler.fail = null
+handler.exp = 20
+
 module.exports = handler
+
+//gtw work kga klo kga work ubah sendiri aja commandnya
