a38fbd762f78033dbfc8f765dc099c2ff6a494d0
ImByu <94290017+ImByu@users.noreply.github.com>
2021-12-04 19:44:23 +0700

    Add files via upload

--- plugins/pacarowner.js
+++ plugins/pacarowner.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6283144570377', 'Raku Gans', m)
-  conn.sendContact(m.chat, '6281228582571' , 'Clara Cans', m)
-  conn.reply(m.chat, `Tuh Pacar Ku Jangan Galak² Ya >_<, Nanti Ku Banned Nanges`, m) 
+  conn.sendContact(m.chat, '-' , '-', m)
+  conn.reply(m.chat, `Ownernya Jomblo Kak:v`, m) 
 }
 handler.help = ['pacarowner']
 handler.tags = ['info']
